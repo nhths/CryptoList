11e597611306c82765e39b9b8443875f46bd165f
Nikita Koschin <nik.koshin@mail.ru>
2022-09-24 21:20:36 +0300

    add crypto list and crypto detail screens

---
+++ app/src/main/java/io/github/nhths/cryptolist/ui/screens/CryptoDetailsScreen.kt
@@ -0,0 +1,9 @@
+package io.github.nhths.cryptolist.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CryptoDetailsScreen(){
+    Text(text = "crypto details")
+}
---
+++ app/src/main/java/io/github/nhths/cryptolist/ui/screens/CryptoListScreen.kt
@@ -0,0 +1,9 @@
+package io.github.nhths.cryptolist.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CryptoListScreen(){
+    Text(text = "crypto list")
+}
