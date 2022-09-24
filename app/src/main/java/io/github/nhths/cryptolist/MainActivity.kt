cef89d615101b07dc30158c7a9e2cf6a27cc87c5
Nikita Koschin <nik.koshin@mail.ru>
2022-09-24 21:14:28 +0300

    add main whole app screen entry

--- app/src/main/java/io/github/nhths/cryptolist/MainActivity.kt
+++ app/src/main/java/io/github/nhths/cryptolist/MainActivity.kt
@@ -10,34 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.nhths.cryptolist.ui.screens.AppScreen
 import io.github.nhths.cryptolist.ui.theme.CryptoListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CryptoListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            AppScreen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CryptoListTheme {
-        Greeting("Android")
-    }
-}
