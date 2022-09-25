7f51e9f1fec9b9b862a2a3eb6ca7189991002e63
Nikita Koschin <nik.koshin@mail.ru>
2022-09-25 16:42:57 +0300

    add percentage positive and negative colors

--- app/src/main/java/io/github/nhths/cryptolist/ui/theme/Color.kt
+++ app/src/main/java/io/github/nhths/cryptolist/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val GrayMain = Color(0xFFE8E8E8)
 
 val OrangeMain = Color(0xFFFF9F00)
 val OrangeSecondary = Color(0x1FFF9F00)
+
+val GreenPositive = Color(0xFF2A9D8F)
+val RedNegative = Color(0xFFEB5757)
