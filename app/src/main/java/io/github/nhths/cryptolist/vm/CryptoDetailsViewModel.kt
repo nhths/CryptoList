e57283ee3d4683eee42dfcd94fe23dd8507a239e
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 20:29:45 +0300

    add crypto details view model

--- app/src/main/java/io/github/nhths/cryptolist/vm/CryptoDetailsViewModel.kt
+++ app/src/main/java/io/github/nhths/cryptolist/vm/CryptoDetailsViewModel.kt
@@ -1,7 +1,29 @@
 package io.github.nhths.cryptolist.vm
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.nhths.cryptolist.ui.model.CryptoDetailsModel
 
-class CryptoDetailsViewModel : ViewModel() {
+class CryptoDetailsViewModel(val cryptoId: String) : ViewModel() {
 
+    enum class State{
+        LOADING,
+        ERROR,
+        SHOWING
+    }
+
+    private val _state = MutableLiveData<State>(State.LOADING)
+    val state: LiveData<State> = _state
+
+    private val _cryptoDetails = MutableLiveData<CryptoDetailsModel>(CryptoDetailsModel("", "", "","",""))
+    val cryptoDetails: LiveData<CryptoDetailsModel> = _cryptoDetails
+
+    init {
+        //load crypto description
+    }
+
+    fun onRefresh(){
+        _state.postValue(State.LOADING)
+    }
 }
