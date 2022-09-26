c6f853e3c372bdec511d57333ba8b22fb61c67a7
Nikita Koschin <nik.koshin@mail.ru>
2022-09-27 01:07:20 +0300

    Add crypto list downloading logic

--- app/src/main/java/io/github/nhths/cryptolist/data/CryptoListRepository.kt
+++ app/src/main/java/io/github/nhths/cryptolist/data/CryptoListRepository.kt
@@ -3,14 +3,48 @@ package io.github.nhths.cryptolist.data
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import io.github.nhths.cryptolist.data.model.CryptoModel
+import io.github.nhths.cryptolist.data.service.CryptoListService
+import io.github.nhths.cryptolist.utils.API_URL
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class CryptoListRepository {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(API_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    private val cryptoListService = retrofit.create(CryptoListService::class.java)
 
     private val _cryptoList = MutableLiveData<List<CryptoModel>>(emptyList())
     val cryptoList:LiveData<List<CryptoModel>> = _cryptoList
 
-    fun updateList(){
+    private val _listError = MutableLiveData<Throwable>()
+    val listError:LiveData<Throwable> = _listError
 
+    fun updateList(currencyId: String){
+        download(currencyId)
+    }
+
+    fun download(currencyId: String){
+        cryptoListService
+            .getCryptoList(currencyId)
+            .enqueue(object : Callback<List<CryptoModel>> {
+                override fun onResponse(
+                    call: Call<List<CryptoModel>>,
+                    response: Response<List<CryptoModel>>
+                ) {
+                    _cryptoList.postValue(response.body())
+                }
+
+                override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+                    _listError.postValue(t)
+                }
+            })
     }
 
 }
