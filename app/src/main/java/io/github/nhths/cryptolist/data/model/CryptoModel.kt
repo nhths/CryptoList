c7c0d03891b1b23e9805a9c1f586f6054a397d8a
Nikita Koschin <nik.koshin@mail.ru>
2022-09-27 00:03:14 +0300

    Annotate json mapping for fields in crypto model

--- app/src/main/java/io/github/nhths/cryptolist/data/model/CryptoModel.kt
+++ app/src/main/java/io/github/nhths/cryptolist/data/model/CryptoModel.kt
@@ -1,10 +1,25 @@
 package io.github.nhths.cryptolist.data.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class CryptoModel (
+    @field:Json(name = "id")
     val id: String,
+
+    @field:Json(name = "name")
     val name: String,
+
+    @field:Json(name = "symbol")
     val ticker: String,
+
+    @field:Json(name = "current_price")
     val price: Double,
+
+    @field:Json(name = "price_change_percentage_24h")
     val diffPercentage: Double,
+
+    @field:Json(name = "image")
     val imageUrl: String
 )
