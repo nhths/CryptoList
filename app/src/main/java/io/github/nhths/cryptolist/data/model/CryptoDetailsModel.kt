bb0c169a99dd580bde8884e44122f0c39120fc85
Nikita Koschin <nik.koshin@mail.ru>
2022-09-27 01:20:47 +0300

    Add json mapping for crypto details model

--- app/src/main/java/io/github/nhths/cryptolist/data/model/CryptoDetailsModel.kt
+++ app/src/main/java/io/github/nhths/cryptolist/data/model/CryptoDetailsModel.kt
@@ -1,9 +1,45 @@
 package io.github.nhths.cryptolist.data.model
 
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.JsonQualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class ImageUrl
+
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class DescriptionEN
+
+@JsonClass(generateAdapter = true)
 data class CryptoDetailsModel(
+    @field:Json(name = "id")
     val Id: String,
+
+    @field:Json(name = "name")
     val name: String,
+
+    @field:Json(name = "id")
+    @ImageUrl
     val imageUrl: String,
+
+    @field:Json(name = "description")
     val description: String,
-    val categories: String
+
+    @field:Json(name = "categories")
+    val categories: List<String>
 )
+
+@FromJson
+@ImageUrl
+fun fromJsonImageUrl(json: Map<String, Any?>): String {
+    return json.getValue("large") as String
+}
+
+@FromJson
+@DescriptionEN
+fun fromJsonDescription(json: Map<String, Any?>): String {
+    return json.getValue("en") as String
+}
