4e9e470b9491ca21062d1d73b2bd74b5cc07bd87
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 16:18:14 +0300

    change Crypto item model, optimizing CryptoItem

--- app/src/main/java/io/github/nhths/cryptolist/ui/model/CryptoItemModel.kt
+++ app/src/main/java/io/github/nhths/cryptolist/ui/model/CryptoItemModel.kt
@@ -1,13 +1,11 @@
 package io.github.nhths.cryptolist.ui.model
 
-import coil.compose.AsyncImagePainter
-
-data class CryptoItemModel (
+data class CryptoItemModel(
     val id: String,
     val name: String,
     val ticker: String,
     val currencySymbol: String,
     val price: Double,
     val diffPercentage: Double,
-    val imagePainter: AsyncImagePainter,
+    val imageUrl: String,
     )
