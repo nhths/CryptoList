fd53bfee764213e2230af86497c63ad8ef856e85
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 17:07:47 +0300

    add state live datas in view model

--- app/src/main/java/io/github/nhths/cryptolist/vm/CryptoListViewModel.kt
+++ app/src/main/java/io/github/nhths/cryptolist/vm/CryptoListViewModel.kt
@@ -1,10 +1,15 @@
 package io.github.nhths.cryptolist.vm
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.accompanist.swiperefresh.SwipeRefreshState
 import io.github.nhths.cryptolist.ui.model.CryptoItemModel
 import io.github.nhths.cryptolist.ui.model.CurrencyItemModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CryptoListViewModel() : ViewModel() {
 
@@ -14,33 +19,40 @@ class CryptoListViewModel() : ViewModel() {
         SHOWING
     }
 
-    private val _state = MutableLiveData<State>()
+    private val _state = MutableLiveData<State>(State.LOADING)
     val state:LiveData<State> = _state
 
+    private val _refreshState = MutableLiveData<SwipeRefreshState>(SwipeRefreshState(false))
+    val refreshState: LiveData<SwipeRefreshState> = _refreshState
+
+    private val _snackBarState = MutableLiveData<SnackbarHostState>(SnackbarHostState())
+    val snackbarState: LiveData<SnackbarHostState> = _snackBarState
+
     //stub
-    private val _cryptoList = MutableLiveData<List<CryptoItemModel>>()
+    private val _cryptoList = MutableLiveData<List<CryptoItemModel>>(emptyList())
     val cryptoList: LiveData<List<CryptoItemModel>> = _cryptoList
 
-    private val _currencyList = MutableLiveData<List<CurrencyItemModel>>()
+    private val _currencyList = MutableLiveData<List<CurrencyItemModel>>(emptyList())
     val currencyList: LiveData<List<CurrencyItemModel>> = _currencyList
 
-    private val _selectedCurrency: MutableLiveData<CurrencyItemModel> = MutableLiveData<CurrencyItemModel>()
-    val selectedCurrency: LiveData<CurrencyItemModel> = _selectedCurrency
-
-    init {
-        _state.postValue(State.SHOWING)
-    }
+    private val _selectedCurrency: MutableLiveData<Int> = MutableLiveData<Int>(0)
+    val selectedCurrency: LiveData<Int> = _selectedCurrency
 
     fun onListUpdate(){
-
+        _refreshState.value!!.isRefreshing = true
+        viewModelScope.launch {
+            delay(3000)
+            snackbarState.value!!.showSnackbar("dfsd")
+            _refreshState.value!!.isRefreshing = false
+        }
     }
 
     fun onItemSelected(item: CryptoItemModel){
         
     }
 
-    fun onCurrencySelected(item: CurrencyItemModel){
-        _selectedCurrency.value = item
+    fun onCurrencySelected(pos: Int, item: CurrencyItemModel){
+        _selectedCurrency.postValue(pos)
     }
 
     override fun onCleared() {
