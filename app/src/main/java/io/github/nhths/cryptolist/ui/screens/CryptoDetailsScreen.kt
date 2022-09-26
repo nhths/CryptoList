f2f3963bdd8925a9bd352f302b3caf7a26c26e31
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 18:21:04 +0300

    add crypto details view model

--- app/src/main/java/io/github/nhths/cryptolist/ui/screens/CryptoDetailsScreen.kt
+++ app/src/main/java/io/github/nhths/cryptolist/ui/screens/CryptoDetailsScreen.kt
@@ -2,8 +2,11 @@ package io.github.nhths.cryptolist.ui.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import io.github.nhths.cryptolist.vm.CryptoDetailsViewModel
 
 @Composable
-fun CryptoDetailsScreen(){
+fun CryptoDetailsScreen(
+    viewModel: CryptoDetailsViewModel = CryptoDetailsViewModel()
+){
     Text(text = "crypto details")
 }
---
+++ app/src/main/java/io/github/nhths/cryptolist/vm/CryptoDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package io.github.nhths.cryptolist.vm
+
+import androidx.lifecycle.ViewModel
+
+class CryptoDetailsViewModel : ViewModel() {
+
+}
