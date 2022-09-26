49304ff4b48b150a5a1a7d89d32576376d643580
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 19:28:10 +0300

    add crypto details model

---
+++ app/src/main/java/io/github/nhths/cryptolist/ui/model/CryptoDetailsModel.kt
@@ -0,0 +1,9 @@
+package io.github.nhths.cryptolist.ui.model
+
+data class CryptoDetailsModel(
+    val Id: String,
+    val name: String,
+    val imageUrl: String,
+    val description: String,
+    val categories: String
+)
