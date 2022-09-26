8d8bcdf221220ceec997a6c20cf190b6fec3a5c4
Nikita Koschin <nik.koshin@mail.ru>
2022-09-26 14:47:55 +0300

    add shadow support for toolbar

--- app/src/main/java/io/github/nhths/cryptolist/ui/screens/components/ToolBar.kt
+++ app/src/main/java/io/github/nhths/cryptolist/ui/screens/components/ToolBar.kt
@@ -1,11 +1,16 @@
 package io.github.nhths.cryptolist.ui.screens.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import io.github.nhths.cryptolist.ui.theme.TitleTextToolbarStyle
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -14,6 +19,7 @@ fun ToolBar(
     title: String,
     canNavigateUp: Boolean = false,
     backgroundColor: Color? = null,
+    shadow: Dp = 3.dp,
     onClickBack: () -> Unit = {}
 ){
     val colors =
@@ -23,7 +29,6 @@ fun ToolBar(
             )
         }
             ?: TopAppBarDefaults.largeTopAppBarColors()
-
 
     TopAppBar(
         title = {
@@ -42,7 +47,8 @@ fun ToolBar(
                 }
             }
         },
-        colors = colors
+        colors = colors,
+        modifier = Modifier.shadow(shadow).padding(bottom = shadow) //without padding, shadow not visible
     )
 }
 
